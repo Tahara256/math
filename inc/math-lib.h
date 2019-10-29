c5483d658a5b431340fbf1e1396de4bf7ceba5ee
Tahara <33447713+Tahara256@users.noreply.github.com>
2019-10-30 04:35:15 +0900

    Add vector to include file

--- inc/math-lib.h
+++ inc/math-lib.h
@@ -4,5 +4,10 @@
 
 /*-- math header files --*/
 
-/* 二次元ベクトルクラス(浮動小数点数型) */
+/* ベクトルクラス(浮動小数点数型) */
+// 二次元
 #include "math/vector-2.h"
+// 三次元
+#include "math/vector-3.h"
+// 四次元
+#include "math/vector-4.h"
